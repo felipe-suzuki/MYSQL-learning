e698be8ba499829fe962b91784e1b9c7098fbaed
felipe-suzuki <94309848+felipe-suzuki@users.noreply.github.com>
2022-07-03 19:56:35 -0300

    Add files via upload

--- Learning SQL - Alan Beaulieu/Chapter 04 Files/04-002-Building A Condition.sql
+++ Learning SQL - Alan Beaulieu/Chapter 04 Files/04-002-Building A Condition.sql
@@ -74,3 +74,81 @@ FROM
 WHERE
     last_name BETWEEN 'FA' AND 'FR';
 
+# locate all films that have a rating of either 'G' or 'PG'
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating = 'G' OR rating = 'PG';
+    
+# Instead of using OR separating conditions, is easier to use WHERE/IN
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating IN ('G' , 'PG');
+    
+-- USING SUBQUERIES -------------------------------------------------
+# The subquery returns the set 'G' and 'PG', and the main query checks to see whether
+# the value of the rating column can be found in the set returned by the subquery.
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating IN (SELECT 
+            rating
+        FROM
+            film
+        WHERE
+            title LIKE '%PET%');
+
+# Using NOT IN o see whether the expression does not exist within the set
+SELECT 
+    title, rating
+FROM
+    film
+WHERE
+    rating NOT IN ('PG-13' , 'R', 'NC-17');
+    
+-- MATCHING CONDITIONS --------------------------------------------------
+# Using wildcards
+-- When searching for partial string matches, you might be interested in:
+-- • Strings beginning/ending with a certain character
+-- • Strings beginning/ending with a substring
+-- • Strings containing a certain character anywhere within the string
+-- • Strings containing a substring anywhere within the string
+-- • Strings with a specific format, regardless of individual characters
+
+-- Wildcard character 		Matches
+-- _ 						Exactly one character
+-- % 						Any number of characters (including 0)
+
+# Query specifies strings containing an A in the second position and a 
+# T in the fourth position, followed by any number of characters and ending in S
+SELECT 
+    last_name, first_name
+FROM
+    customer
+WHERE
+    last_name LIKE '_A_T%S';
+    
+# This query finds all customers whose last name begins with Q or Y.
+SELECT 
+    last_name, first_name
+FROM
+    customer
+WHERE
+    last_name LIKE 'Q%'
+        OR last_name LIKE 'Y%';
+
+-- USING REGULAR EXPRESSIONS REGEXP -----------------------------------------
+# This query finds all customers whose last name begins with Q or Y using REGEXP.
+SELECT 
+    last_name, first_name
+FROM
+    customer
+WHERE
+    last_name REGEXP '^[QY]';
