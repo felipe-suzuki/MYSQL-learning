166422d58d38b14f11306a923c3d0fd99c6c9da6
felipe-suzuki <94309848+felipe-suzuki@users.noreply.github.com>
2022-07-11 22:07:32 -0300

    Add files via upload

---
+++ Learning SQL - Alan Beaulieu/Chapter 07 Files/07-EX01.sql
@@ -0,0 +1,7 @@
+-- Write a query that returns the 17th through 25th characters of the string 'Please
+-- find the substring in this string'.
+
+SELECT 
+    SUBSTRING('Please find the substring in this string',
+        17,
+        9) AS Substring;
