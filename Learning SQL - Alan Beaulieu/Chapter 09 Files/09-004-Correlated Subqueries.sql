af559028b0be8034aa342f8737d64fb3318136f8
felipe-suzuki <94309848+felipe-suzuki@users.noreply.github.com>
2022-11-23 21:55:02 -0300

    Add files via upload

--- Learning SQL - Alan Beaulieu/Chapter 09 Files/09-004-Correlated Subqueries.sql
+++ Learning SQL - Alan Beaulieu/Chapter 09 Files/09-004-Correlated Subqueries.sql
@@ -36,3 +36,84 @@ WHERE
             payment AS p
         WHERE
             p.customer_id = c.customer_id) BETWEEN 180 AND 240;
+            
+-- The exists Operator --
+-- You use the exists operator when you want to identify that a
+-- relationship exists without regard for the quantity.
+
+-- The following query finds all the customers who rented at least one 
+-- film prior to May 25, 2005, without regard for how many films were rented:
+SELECT 
+    c.first_name, c.last_name
+FROM
+    customer AS c
+WHERE
+    EXISTS( SELECT 
+            1
+        FROM
+            rental AS r
+        WHERE
+            r.customer_id = c.customer_id
+                AND DATE(r.rental_date) < '2005-05-25');
+                
+-- Using the exists operator, your subquery can return zero, one, or many rows, and
+-- the condition simply checks whether the subquery returned one or more rows.
+
+-- You may also use not exists to check for subqueries that return no rows, as 
+-- demonstrated by the following:
+SELECT 
+    a.first_name, a.last_name
+FROM
+    actor a
+WHERE
+    NOT EXISTS( SELECT 
+            1
+        FROM
+            film_actor fa
+                INNER JOIN
+            film f ON f.film_id = fa.film_id
+        WHERE
+            fa.actor_id = a.actor_id
+                AND f.rating = 'R');
+
+-- Data Manipulation Using Correlated Subqueries --
+
+-- Subqueries are used heavily in update, delete, and insert statements 
+-- as well, with correlated subqueries appearing frequently in update and 
+-- delete statements. 
+-- Here’s an example of a correlated subquery used to modify the last_update 
+-- column in the customer table:
+UPDATE customer AS c 
+SET 
+    c.last_update = (SELECT 
+            MAX(r.rental_date)
+        FROM
+            rental AS r
+        WHERE
+            r.customer_id = c.customer_id);
+
+-- Check if every customer will have at least one film rental before attempting to 
+-- update the last_update column; otherwise, -- the column will be set to null, 
+-- since the subquery would return no rows.
+UPDATE customer AS c 
+SET 
+    c.last_update = (SELECT 
+            MAX(r.rental_date)
+        FROM
+            rental AS r
+        WHERE
+            r.customer_id = c.customer_id)
+WHERE
+    EXISTS( SELECT 
+            1
+        FROM
+            rental AS r
+        WHERE
+            r.customer_id = c.customer_id);
+            
+-- Correlated subqueries are also common in delete statements.
+DELETE FROM customer AS c
+WHERE 365 < ALL
+	(SELECT datediff(NOW(), r.rental_date) DAYS_SINCE_LAST_RENTAL
+    FROM rental AS r
+    WHERE r.customer_id = c.customer_id);
