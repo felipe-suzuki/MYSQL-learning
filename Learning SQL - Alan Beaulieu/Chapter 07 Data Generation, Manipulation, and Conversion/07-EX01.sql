86a662339f33693a0356ff9baad1eea0491ec3e8
felipe-suzuki <94309848+felipe-suzuki@users.noreply.github.com>
2022-11-25 22:44:04 -0300

    Delete Learning SQL - Alan Beaulieu/Chapter 07 Data Generation, Manipulation, and Conversion directory

--- Learning SQL - Alan Beaulieu/Chapter 07 Data Generation, Manipulation, and Conversion/07-EX01.sql
+++
@@ -1,7 +0,0 @@
--- Write a query that returns the 17th through 25th characters of the string 'Please
--- find the substring in this string'.
-
-SELECT 
-    SUBSTRING('Please find the substring in this string',
-        17,
-        9) AS Substring;
--- Learning SQL - Alan Beaulieu/Chapter 07 Data Generation, Manipulation, and Conversion/07-EX03.sql
+++
@@ -1,7 +0,0 @@
--- Write a query to return just the month portion of the current date.
-
-SELECT MONTH(CURRENT_DATE());
-
- -- THE BOOK SOLUTION, SAME RESULT
- 
- SELECT EXTRACT(MONTH FROM CURRENT_DATE());
