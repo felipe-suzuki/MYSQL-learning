59af8cdf30b5bafe30921ad38866a5d6e4ce17a7
felipe-suzuki <94309848+felipe-suzuki@users.noreply.github.com>
2022-11-25 22:43:31 -0300

    Delete Learning SQL - Alan Beaulieu/Chapter 04 Filtering directory

--- Learning SQL - Alan Beaulieu/Chapter 04 Filtering/04-EX03.sql
+++
@@ -1,9 +0,0 @@
-# Construct a query that retrieves all rows from the payments table
-# where the amount is either 1.98, 7.98, or 9.98.
-
-SELECT 
-    *
-FROM
-    payment
-WHERE
-    amount IN (1.98 , 7.98, 9.98);
